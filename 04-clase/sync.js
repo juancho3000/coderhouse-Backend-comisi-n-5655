fee3dd82332e1d8d577595cb177cd8d28ff9608f
Juan Suarez <jsuarez0698@gmail.com>
2023-12-19 22:21:05 -0400

    haciendo cambios sprint2

---
+++ 04-clase/sync.js
@@ -0,0 +1,30 @@
+//import ProductManager from "../primera-entrega/management";
+
+//crear archivos sincronos
+const fs = require("fs");
+const ruta = "./04-clase/data/tickets.json"
+const info = JSON.stringify([
+    {
+        title: "product1",
+        category: "firstLine",
+        price: "10$" 
+    }, 
+    {
+        title: "product2",
+        category: "secondLina",
+        price: "$20"
+    }
+], null,2)
+fs.writeFileSync(ruta, info);
+//crear archivos sincronos
+
+//leer archivos de forma asincrona
+let configuracion = "utf-8"
+const dataReading = fs.readFileSync(ruta, configuracion);
+const convertData = JSON.parse(dataReading)
+console.log(convertData);
+//leer archivos de forma 
+
+//eliminar archivos de manera sincrona
+
+//eliminar archivos de manera sincrona

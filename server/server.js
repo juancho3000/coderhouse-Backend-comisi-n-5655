eef1d269b900b32b50b12dfed1ccb6e2af1ff797
Juan Suarez <jsuarez0698@gmail.com>
2023-12-25 19:13:11 -0400

    subida para comprobar 2

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const server = http.createServer();
 
 const PORT = 8080;
 
-const callBack = () => console.log("servidor listo en puerto:" + PORT);
+const callBack = () => console.log("servidor listo ahora mismo en puerto:" + PORT);
+console.log("revisar cambios " + PORT)
 
 server.listen(PORT, callBack);

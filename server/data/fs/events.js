fcdcf802cb7cda692fb4fad96d77ddde292e8783
Juan Suarez <jsuarez0698@gmail.com>
2023-12-28 01:00:23 -0400

    agregando cambios a sprint3

--- server/data/fs/events.js
+++ server/data/fs/events.js
@@ -19,13 +19,13 @@ class EventsManager {
         return error.message
        }
     }
-
     constructor (path) {
         this.path = path;
         this.events = []
         this.init();
     }
 
+    //creating data - begining
     async createEvent(data){
         try{
             if(!data.name || !data.place){
@@ -35,7 +35,7 @@ class EventsManager {
             id: crypto.randomBytes(12).toString("hex"),
             name: data.name,
             place: data.place,
-            price: data.price || 10,
+            price: data.price || "$" + 10,
             capacity: data.capacity || 50,
             date: data.date || new Date()
         }
@@ -44,16 +44,106 @@ class EventsManager {
         await fs.promises.writeFile(this.path, jsonData);
         console.log(event.id);
         return event.id
-        } catch(err){
-            console.log(err.message);
-            return err.message;
+        } catch(error){
+            console.log(error.message);
+            return error.message;
+        } 
+    }
+    //creating data - end
+
+    //reading data - begining
+    readEevents() {
+        try{
+            if(this.events.length === 0){
+                throw new Error("there is no data");
+            } else {
+               console.log(this.events);
+            return this.events 
+            } 
+        } catch (error) {
+            console.log(error.message);
+            return error.message;
         }
-        
+    }
+    //reading data - end
+
+    //reading single selected id data - begining
+    readEeventById(id) {
+    try{
+        const one = this.events.find(each => each.id === id)
+        if(!one){
+            throw new Error("no events found with id:" + id)
+        }else{
+            console.log("read event id:" + id);
+            return one;
+        }
+    }catch(error){
+        console.log(error.message);
+        return error.message
     }
 }
+//reading single selected id data - begining
+
+/* //removeEvent, I just don't want to touch it
+async removeEventById(id){
+    try{
+        let one = this.events.find(each => each.id === id)
+        if(!one) {
+            throw new Error("there isn't any event to delete")
+        } else {
+           this.events = this.events.filter(each => each.id !== id);
+           const jsonData = JSON.stringify(this.events, null, 2);
+           await fs.promises.writeFile(this.path, jsonData);
+           console.log("deleted id:" + id);
+           return id;
+        }
+    }catch (error) {
+        console.log(error.message);
+        return error.message;
+    }
+}*/  //removeEvent, I just don't want to touch it
+
+//operation to sell tickets according to capacity - begining
+async soldTicket (qty, eventId) {
+    try{
+        const one = this.readEeventById(eventId);
+        if(one){
+            if(one.capacity >= qty) {
+                 one.capacity = one.capacity - qty
+        EventsManager.#totalGain = EventsManager.#totalGain + one.price * qty * EventsManager.#perGain
+
+        const jsonData = JSON.stringify(this.events, null, 2)
+        await fs.promises.writeFile(this.path, jsonData)
+        console.log("capacity available:" + one.capacity);
+        return one.capacity;
+            } else {
+                
+            }
+        }
+
+    } catch (error) {
+        console.log("no product sold", error.message);
+        return error.message
+    }
+    
+}
+//operation to sell tickets according to capacity - end
+
+}
+
+
+//events list - begining
 
 const events = new EventsManager("./server/data/fs/files/events.json");
+events.readEevents();
 events.createEvent({name: "house1" , place: "urb"});
-events.createEvent({name: "apt1" , place: "buildings", price: "$60000", capacity: "5"});
-events.createEvent({name: "mansion1", price:"$500000", capacity: "20"});
+events.createEvent({name: "apt1" , place: "buildings", price: "$60000", capacity: 5});
+events.createEvent({name: "mansion1", place: "mansions", price: "$500000", capacity: 20});
+events.readEevents();
+events.readEeventById(3);
+events.readEeventById("42f13d29362e51286a2a2541");
+//events.removeEventById("42f13d29362e51286a2a2541")
+events.soldTicket(10, "07312b42fa7b1672cd5a6311");
+
+//events list - begining
 

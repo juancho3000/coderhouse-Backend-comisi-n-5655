02d628ac6b49ea412c433bf64c47050298a9950e
Juan Suarez <jsuarez0698@gmail.com>
2024-01-09 23:56:39 -0400

    sprint3 save

--- server/data/fs/events.js
+++ server/data/fs/events.js
@@ -3,63 +3,63 @@ import crypto from "crypto";
 class EventsManager {
     static #perGain = 0.3;
     static #totalGain = 0;
-    events = []
-    init(){ 
-        try{
-       const exists = fs.existsSync(this.path);
-       console.log(exists);
-        if(!exists){
-            const data = JSON.stringify([], null, 2)
-            fs.writeFileSync(this.path, data)
-        }else{
-            this.events = JSON.parse(fs.readFileSync(this.path, "utf-8"))
-            console.log(all);
+    //events = []
+    init() {
+        try {
+            const exists = fs.existsSync(this.path);
+            console.log(exists);
+            if (!exists) {
+                const data = JSON.stringify([], null, 2)
+                fs.writeFileSync(this.path, data)
+            } else {
+                this.events = JSON.parse(fs.readFileSync(this.path, "utf-8"))
+            }
+        } catch (error) {
+            return error.message
         }
-       }catch(error){
-        return error.message
-       }
     }
-    constructor (path) {
+    constructor(path) {
         this.path = path;
-        this.events = []
+        this.events = [];
         this.init();
     }
 
     //creating data - begining
-    async createEvent(data){
-        try{
-            if(!data.name || !data.place){
-                throw new Error("name and place are obligatory")
+    async createEvent(data) {
+        try {
+            if (!data.name || !data.place) {
+                throw new Error("name and place are obligatory");
             }
             const event = {
-            id: crypto.randomBytes(12).toString("hex"),
-            name: data.name,
-            place: data.place,
-            price: data.price || "$" + 10,
-            capacity: data.capacity || 50,
-            date: data.date || new Date()
-        }
-        this.events.push(event);
-        const jsonData = JSON.stringify(this.events, null, 2);
-        await fs.promises.writeFile(this.path, jsonData);
-        console.log(event.id);
-        return event.id
-        } catch(error){
+                id: crypto.randomBytes(12).toString("hex"),
+                name: data.name,
+                place: data.place,
+                price: data.price || "$" + 10,
+                capacity: data.capacity || 50,
+                date: data.date || new Date(),
+            };
+            this.events.push(event);
+            const jsonData = JSON.stringify(this.events, null, 2);
+
+            await fs.promises.writeFile(this.path, jsonData);
+            fs.promises.readFile("create:" + event.id);
+            return event.id;
+        } catch (error) {
             console.log(error.message);
             return error.message;
-        } 
+        }
     }
     //creating data - end
 
     //reading data - begining
     readEvents() {
-        try{
-            if(this.events.length === 0){
+        try {
+            if (this.events.length === 0) {
                 throw new Error("there is no data");
             } else {
-               console.log(this.events);
-            return this.events 
-            } 
+                console.log(this.events);
+                return this.events
+            }
         } catch (error) {
             console.log(error.message);
             return error.message;
@@ -69,58 +69,62 @@ class EventsManager {
 
     //reading single selected id data - begining
     readEeventById(id) {
-    try{
-        const one = this.events.find(each => each.id === id)
-        if(!one){
-            throw new Error("no events found with id:" + id)
-        }else{
-            console.log("read event id:" + id);
-            return one;
-        }
-    }catch(error){
-        console.log(error.message);
-        return error.message
-    }
-}
-//reading single selected id data - begining
-
-//operation to sell tickets according to capacity - begining
-async soldTicket (qty, eventId) {
-    try{
-        const one = this.readEeventById(eventId);
-        if(one){
-            if(one.capacity >= qty) {
-                 one.capacity = one.capacity - qty
-        EventsManager.#totalGain = EventsManager.#totalGain + one.price * qty * EventsManager.#perGain
-
-        const jsonData = JSON.stringify(this.events, null, 2)
-        await fs.promises.writeFile(this.path, jsonData)
-        console.log("capacity available:" + one.capacity);
-        return one.capacity;
+        try {
+            const one = this.events.find(each => each.id === id)
+            if (!one) {
+                throw new Error("no events found with id:" + id)
             } else {
-                
+                console.log("read event id:" + id);
+                return one;
             }
+        } catch (error) {
+            console.log(error.message);
+            return error.message
+        }
+    }
+    //reading single selected id data - begining
+
+    //operation to sell tickets according to capacity - begining
+    async soldTicket(qty, eventId) {
+        try {
+            const one = this.readEeventById(eventId);
+            if (one) {
+                if (one.capacity >= qty) {
+                    one.capacity = one.capacity - qty
+                    EventsManager.#totalGain = EventsManager.#totalGain + one.price * qty * EventsManager.#perGain
+
+                    const jsonData = JSON.stringify(this.events, null, 2)
+                    await fs.promises.writeFile(this.path, jsonData)
+                    console.log("capacity available:" + one.capacity);
+                    return one.capacity;
+                } else {
+
+                }
+            }
+
+        } catch (error) {
+            console.log("no product sold", error.message);
+            return error.message
         }
 
-    } catch (error) {
-        console.log("no product sold", error.message);
-        return error.message
     }
-    
-}
-//operation to sell tickets according to capacity - end
+    //operation to sell tickets according to capacity - end
 
 }
 const events = new EventsManager("./server/data/fs/files/events.json");
+const route = "./server/data/fs/files/events.json"
+let config = "utf-8";
+fs.promises.readFile(route, config)
+    .then(res => console.log("read events is:", JSON.parse(res)))
+    .catch(err => console.log("error promise read" + err));
 export default events;
-
 //events list - begining
 
 /*events.readEevents();
 events.createEvent({name: "house1" , place: "urb"});
 events.createEvent({name: "apt1" , place: "buildings", price: "$60000", capacity: 5});
 events.createEvent({name: "mansion1", place: "mansions", price: "$500000", capacity: 20});
-events.readEevents();
+
 events.readEeventById(3);
 events.readEeventById("42f13d29362e51286a2a2541");
 //events.removeEventById("42f13d29362e51286a2a2541")
--- server/server.js
+++ server/server.js
@@ -6,42 +6,108 @@ const server = express();
 const PORT = 8080;
 
 const callBack = () => console.log("servidor listo ahora mismo en puerto:" + PORT);
-server.use(express.urlencoded({extended:true}));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 server.listen(PORT, callBack);
 
 //endpoints
 
-server.get("/api/events", (req, res) => {
-    try{
-        const all = events.readEvents();
-        if(Array.isArray(all)){
-            return res.status(200).json({
-                success: true,
-                responsse: all,
+//api/products post
+server.post("/api/products", async (req, res) => {
+    try {
+        const data = req.body;
+        const response = await events.createEvent(data);
+        if (response === "name and place are obligatory") {
+            return res.json({
+                statusCode: 400,
+                message: response,
             });
         } else {
-            return res.status(404).json({
-                success: false,
-                message: all,
-            })
+            return res.json({
+                statusCode: 201,
+                message: "successfully created",
+                response
+            });
         }
 
     } catch (error) {
-        return res.status(500).json({
-            success: false,
+        console.log(error);
+        return res.json({
+            statusCode: 500,
             message: error.message,
-        })
+        });
     }
-})
+});
+//api/products post
+
+server.get("/api/products", async (req, res) => {
+    try {
+        const all = await events.readEvents()
+        if (Array.isArray(all)) {
+            return res.json({
+                statusCode: 200,
+                response: all
+            });
+        } else {
+            return res.json({
+                statusCode: 400,
+                message: all
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            statusCode: 500,
+            message: error.message,
+        });
+    }
+});
+
+server.get("/api/products/:eid", async (req, res) => {
+    try {
+        const { eid } = req.params;
+        const productId = await events.readEeventById(eid);
+        if (typeof productId === "string") {
+            return res.json({
+                statusCode: 404,
+                message: productId
+            });
+        } else {
+            return res.json({
+                statusCode: 200,
+                responsse: productId
+            });
+        };
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            statusCode: 500,
+            message: error.message,
+        });
+    };
+});
+
+/*server.put("/api/products/:eid/:quantity", async (req, res) => {
+    try {
+        const {eid, quantity} = req.params
+        const twoParams = await 
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            statusCode: 500,
+            message: error.message,
+        });
+    }
+})*/
 
 server.get("/list", (req, res) => {
-    try{
-        const listData =2 /*[
+    try {
+        const listData = 2 /*[
             {name: "juan", id: 1, job: "engineer"},
             {name: "carlos", id: 1, job: "doctor"},
             {name: "beliana", id: 1, job: "lawyer"},
         ];*/
-        if(Array.isArray(listData)){
+        if (Array.isArray(listData)) {
             return res.status(200).json({
                 success: true,
                 responsse: listData,
@@ -61,8 +127,8 @@ server.get("/list", (req, res) => {
     }
 })
 
-server.get("/api/events/:eid", (req, res) => {
-    const {eid} = req.params;
+server.get("/api/products/:eid", (req, res) => {
+    const { eid } = req.params;
     const oneId = events.readEeventById(eid);
     return res.status(200).json(oneId);
 })
@@ -73,7 +139,7 @@ const cbRoute = (req, res) => {
 };
 server.get(ruta, cbRoute);
 
-const ruta2 = "/events"
+const ruta2 = "/products"
 const cbRoute2 = (req, res) => {
     const products = ["juan", "suarez"];
     return res.status(200).send(products)
@@ -83,24 +149,24 @@ server.get(ruta2, cbRoute2);
 const rutaApi = "/api/users"
 const cbRouteApi = (req, res) => {
     const users = [
-        {name: "alfredo" , id: 1, email: "alfg87@email.com"},
-        {name: "raquel" , id: 2, email: "raqe95@email.com"},
-        {name: "asier" , id: 3, email: "asp01@email.com"},
-]
-return res.status(200).json(users);
+        { name: "alfredo", id: 1, email: "alfg87@email.com" },
+        { name: "raquel", id: 2, email: "raqe95@email.com" },
+        { name: "asier", id: 3, email: "asp01@email.com" },
+    ]
+    return res.status(200).json(users);
 };
 server.get(rutaApi, cbRouteApi);
 
 const rutaParams = "/api/products/:pid"
 const cbRouteParams = (req, res) => {
-    const {pid} = req.params;
+    const { pid } = req.params;
     return res.status(200).send("el ID del producto es:" + pid)
 };
 server.get(rutaParams, cbRouteParams);
 
 const rutaParams2 = "/api/products/:title/:category/:price/:stock"
 const cbRouteParams2 = (req, res) => {
-    const {title, category, price, stock} = req.params;
-    return res.status(200).json({ title,category,price,stock })
+    const { title, category, price, stock } = req.params;
+    return res.status(200).json({ title, category, price, stock })
 };
 server.get(rutaParams2, cbRouteParams2);
